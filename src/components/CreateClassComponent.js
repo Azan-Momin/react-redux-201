3996b18eb82c34cb838d8a4360bda9b7e1f805d0
Azan Momin <azan.momin@gmail.com>
2023-05-28 21:54:31 +0530

    completed 201 11.6

---
+++ src/components/CreateClassComponent.js
@@ -0,0 +1,13 @@
+var React = require("react");
+var createReactClass = require("create-react-class");
+
+var CreateClassComponent = createReactClass({
+    render: function () {
+        return (
+            <h1>In CreateClassComponent</h1>
+        );
+    }
+});
+
+// module.exports = CreateClassComponent;
+// export default CreateClassComponent;
