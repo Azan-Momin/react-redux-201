6d41ae95529421f3b6b1950132fc063781fdd424
azan-momin <34938345+Azan-Momin@users.noreply.github.com>
2023-06-05 11:03:02 +0000

    201 11 completed

--- src/api/authorApi.js
+++ src/api/authorApi.js
@@ -1,8 +1,8 @@
 import { handleResponse, handleError } from "./apiUtils";
-const baseUrl = process.env.API_URL + "/authors/";
+// const baseUrl = process.env.API_URL + "/authors/";
+const baseUrl = "https://bzefp6-3001.csb.app/authors/"; // Hard-coded for code-sandbox
 
 export function getAuthors() {
-  return fetch(baseUrl)
-    .then(handleResponse)
-    .catch(handleError);
+  console.log("Hard-coded the base-url in this file");
+  return fetch(baseUrl).then(handleResponse).catch(handleError);
 }
