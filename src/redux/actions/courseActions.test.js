cf306a160c2fc865057d1a3dddd750ef9239b488
azan-momin <34938345+Azan-Momin@users.noreply.github.com>
2023-08-16 13:57:40 +0000

    Update courseActions.test.js

--- src/redux/actions/courseActions.test.js
+++ src/redux/actions/courseActions.test.js
@@ -1,6 +1,15 @@
 import * as courseActions from "./courseActions";
 import * as types from "./actionTypes";
 import { courses } from "../../../tools/mockData";
+import thunk from "redux-thunk";
+import fetchMock from "fetch-mock";
+import configureMockStore from "redux-mock-store";
+
+// test an async action
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+
+// describe...
 
 describe("createCourseSuccess", () => {
   it("should create a CREATE_COURSE_SUCCESS action", () => {
